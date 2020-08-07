05ef40e3e270265e3766324b27ed5fefab99a3b3
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-07 10:46:32 -0700

    fixed g in comp.py and completed cityreader.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import sys
+import csv
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return f"{self.name}, {self.lat}, {self.lon}"
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -15,6 +26,7 @@
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 cities = []
+print(f"full sys.argv is {sys.argv[0]}")
 
 
 def cityreader(cities=[]):
@@ -22,7 +34,11 @@ def cityreader(cities=[]):
     # Ensure that the lat and lon valuse are all floats
     # For each city record, create a new City instance and add it to the
     # `cities` list
-
+    with open('cities.csv', newline='') as csvfile:
+        f = csv.reader(csvfile)
+        for n in f:
+            if not n[0] == "city":
+                cities.append(City(n[0], float(n[3]), float(n[4])))
     return cities
 
 
